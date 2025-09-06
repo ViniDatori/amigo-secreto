05f13d9718494fb080e30e539bab77a6b348ea36
Vinícius Datori <vinidatori@gmail.com>
2025-09-06 19:51:01 -0300

    Refazendo o código.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-// Lista de pessoas que participarão do amigo secreto (programar um limite de 20 pessoas):
+// Lista de pessoas que participarão do amigo secreto:
 
 let amigosIncluidos = [];
-let participantes;
 
 // Função para adicionar amigos ao sorteio:
 
 function adicionar() {
+    // programar um limite de 20 pessoas
     let amigo = document.getElementById("nome-amigo").value;
     amigosIncluidos.push(amigo);
     let listaAmigos = document.getElementById("lista-amigos");
@@ -24,28 +24,6 @@ function sortear(id) {
 
     else if(amigosIncluidos.length % 2 == 0) {
 
-        participantes = amigosIncluidos.length;
-
-        while(participantes != 0) {
-            
-            // Sorteio das duplas (FALTA GARANTIR QUE A PESSOA NÃO CAIA COM ELA MESMA E NENHUMA DUPLA SEJA REPETIDA!)
-            let pessoa1 = Math.floor(Math.random() * amigosIncluidos.length);
-            let pessoa1Sorteada = amigosIncluidos[pessoa1];
-
-            let pessoa2 = Math.floor(Math.random() * amigosIncluidos.length);
-            let pessoa2Sorteada = amigosIncluidos[pessoa2];
-
-            // Adição das duplas sorteadas na tela:
-            let containerSorteados = document.getElementById("container-sorteados");
-            let duplaSorteada = document.createElement("p");
-            duplaSorteada.id = id;
-            duplaSorteada.append(`${pessoa1Sorteada} -> ${pessoa2Sorteada}`);
-            containerSorteados.appendChild(duplaSorteada);
-
-            // Garantia que o loop terá fim:
-            participantes--;
-        }
-
     } else {
         alert("Você precisa incluir mais uma pessoa para realizar o sorteio.");
         return;
@@ -57,5 +35,4 @@ function sortear(id) {
 function reiniciar() {
     amigosIncluidos = [];
     document.getElementById("lista-amigos").textContent = "";
-    document.getElementById("container-sorteados").innerHTML = "";
 }
