9ad6a777d2377fc0c4314297b4cb9e2a3b8cc2ba
Vinícius Datori <vinidatori@gmail.com>
2025-09-06 21:48:08 -0300

    Configurado o sistema de embaralhamento e montagem dos pares.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,56 @@
 // Lista de pessoas que participarão do amigo secreto:
 
 let amigosIncluidos = [];
+let amigosJaSorteados = [];
+let participantes;
 
 // Função para adicionar amigos ao sorteio:
 
 function adicionar() {
-    // programar um limite de 20 pessoas
-    let amigo = document.getElementById("nome-amigo").value;
-    amigosIncluidos.push(amigo);
-    let listaAmigos = document.getElementById("lista-amigos");
-    listaAmigos.textContent = `${amigosIncluidos}`;
-    document.getElementById("nome-amigo").value = "";
+    
+    if(amigosIncluidos.length > 20) {
+        alert("Limite máximo de pessoas atingido!");
+        return;
+    } else {
+        let amigo = document.getElementById("nome-amigo").value;
+        amigosIncluidos.push(amigo);
+        let listaAmigos = document.getElementById("lista-amigos");
+        listaAmigos.textContent = `${amigosIncluidos}`;
+        document.getElementById("nome-amigo").value = "";
+    }
 }
 
 // Função para sortear quem será o amigo secreto de quem:
 
-function sortear(id) {
+function sortear() {
 
     if (amigosIncluidos.length < 2) {
         alert("Você precisa incluir pelo menos 2 pessoas na lista.");
         return;
     }
 
+    // Caso o número de participantes seja par, "embaralhamos os nomes" aleatoriamente em uma nova lista:
+
     else if(amigosIncluidos.length % 2 == 0) {
+        participantes = amigosIncluidos.length;
+        while(participantes != 0) {
+            let amigoAleatorio = amigosIncluidos[Math.floor(Math.random() * amigosIncluidos.length)];
+            if(amigosJaSorteados.includes(amigoAleatorio)) {
+                amigoAleatorio = "";
+                amigoAleatorio = amigosIncluidos[Math.floor(Math.random() * amigosIncluidos.length)];
+            } else {
+                amigosJaSorteados.push(amigoAleatorio);
+                participantes--;
+            }
+        }
+
+        // Após embaralhar, montamos os pares, ou seja, quem tirou quem:
+
+        while(amigosJaSorteados.length != 0) {
+            console.log(`${amigosJaSorteados[amigosJaSorteados.length - 1]} -> ${amigosJaSorteados[amigosJaSorteados.length - 2]}`);
+            amigosJaSorteados.pop(amigosJaSorteados[amigosJaSorteados.length - 1]);
+            amigosJaSorteados.pop(amigosJaSorteados[amigosJaSorteados.length - 2]);
+        }
 
     } else {
         alert("Você precisa incluir mais uma pessoa para realizar o sorteio.");
